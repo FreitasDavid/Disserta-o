7fedbb8ffa552adac19cfba5bf1bce7968ff53a9
David Freitas <dfreitas022@gmail.com>
2022-11-29 19:33:30 -0300

    adicionado arquivo

--- send_results.py
+++ send_results.py
@@ -1,7 +1,36 @@
 import sys
-from mail_delivery import *
+import smtplib
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 from time import sleep
 from glob import glob
+
+def send(from_mail, password, to_mail, subject, body, attachments = None):
+    msg = MIMEMultipart()
+    msg['From'] = from_mail
+    msg['To'] = to_mail
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'plain'))
+    if attachments is not None:
+        for attachment_file in attachments:
+            attachment = open(attachment_file, 'rb')
+            part = MIMEBase('application', 'octet-stream')
+            part.set_payload((attachment).read())
+            encoders.encode_base64(part)
+            part.add_header('Content-Disposition', 'attachment', filename = attachment_file)
+            msg.attach(part)
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(from_mail, password)
+    text = msg.as_string()
+    server.sendmail(from_mail, to_mail, text)
+    server.quit()
 
 if __name__ == '__main__':
     print('Iniciando envio')
